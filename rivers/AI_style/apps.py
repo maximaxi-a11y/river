bfaf019f483cc6bbfd9b257aabc7f251ee860b6b
Maxim <kupotip364@gmail.com>
2024-08-15 12:16:31 +0300

    prot

---
+++ rivers/AI_style/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AiStyleConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'AI_style'
--- rivers/main/urls.py
+++ rivers/main/urls.py
@@ -6,5 +6,4 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.index, name='main'),
-    path('ad_item/', views.create_ad, name='create_ad'),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
