931a6327887f8f8f286abbca90fd06ec2216004f
Maxim <kupotip364@gmail.com>
2024-08-18 14:32:52 +0300

    Fixstore

--- rivers/shop/views.py
+++ rivers/shop/views.py
@@ -5,9 +5,9 @@ from .models import Product
 
 def product_detail(request, product_id):
     product = get_object_or_404(Product, id=product_id)  # Получение товара по его ID
-    return render(request, 'product_detail.html', {'product': product})
+    return render(request, 'dt.html', {'product': product})
 
 def product_list(request):
     products = Product.objects.prefetch_related('images').all()
-    return render(request, 'product_list.html', {'products': products})
+    return render(request, 'tt.html', {'products': products})
 
